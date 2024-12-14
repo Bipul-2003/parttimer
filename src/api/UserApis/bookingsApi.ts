2227d4682b89a830e0c5e534b2d6a9e517207eaa
Bipul Swain <bipulswain365@gmail.com>
2024-12-14 21:56:07 +0530

    Add withCredentials option to user bookings API request

--- src/api/UserApis/bookingsApi.ts
+++ src/api/UserApis/bookingsApi.ts
@@ -4,7 +4,7 @@ const API_URL = "http://localhost:8080/api";
 
 export const getUserPartimeBookings = async () => {
   try {
-    const response = await axios.get(`${API_URL}/bookings/user`);
+    const response = await axios.get(`${API_URL}/bookings/user`,{withCredentials: true});
     return response.data;
   } catch (error) {
     throw new Error("Failed to fetch user bookings");
